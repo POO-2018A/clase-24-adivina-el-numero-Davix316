2015fba639f675db8211ca46753cab5f9792d03c
David Curipoma <38595374+Davix316@users.noreply.github.com>
2018-07-02 09:02:35 -0500

    Add files via upload

---
+++ Numeros/src/numeros/Numeros.java
@@ -0,0 +1,41 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package numeros;
+
+import java.util.Random;
+
+
+/**
+ *
+ * @author ESFOT
+ */
+public class Numeros {
+
+    /**
+     * @param args the command line arguments
+     */
+    
+    int numero;
+    
+    public Numeros(int numero) {
+        this.numero = numero;
+    }
+
+    public int getNumero() {
+        return numero;
+    }
+
+    public void setNumero(int numero) {
+        this.numero = numero;
+    }
+    
+    
+    
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
